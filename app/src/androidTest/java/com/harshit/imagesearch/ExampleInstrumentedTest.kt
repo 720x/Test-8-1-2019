c85bf26ffc11f5fba1c04ca9e96ec9e5e10d9753
720x <107641691+720x@users.noreply.github.com>
2020-06-13 21:44:46 +0000

    Updating the readme file

--- app/src/androidTest/java/com/harshit/imagesearch/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/harshit/imagesearch/ExampleInstrumentedTest.kt
@@ -1,4 +1,9 @@
 package com.harshit.imagesearch
 
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.t
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+imp
