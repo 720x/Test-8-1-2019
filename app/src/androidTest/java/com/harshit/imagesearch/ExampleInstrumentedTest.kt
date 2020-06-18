cc9272a7a89cc0de14aef26380e963f3f616a443
720x <107641691+720x@users.noreply.github.com>
2020-06-18 21:09:22 +0000

    Add eslint config

--- app/src/androidTest/java/com/harshit/imagesearch/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/harshit/imagesearch/ExampleInstrumentedTest.kt
@@ -18,4 +18,5 @@ class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstru
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.harshit.imagesearch", appContext.pack
