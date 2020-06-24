9c6d429ffb46d1bf647b69b573ded7d500c9d36e
720x <107641691+720x@users.noreply.github.com>
2020-06-24 17:55:17 +0000

    fixed redirect

--- app/src/main/java/com/harshit/imagesearch/activity/CartActivity.kt
+++ app/src/main/java/com/harshit/imagesearch/activity/CartActivity.kt
@@ -5,4 +5,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.Rec
+import androidx.recyclerview.widget.RecyclerView
+import butterknife.ButterKnife
+import com.google.firebase.auth.FirebaseAuth
+imp
