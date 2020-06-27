cbeb2bced4d09470c0bbbded4aa4c917f29035d2
720x <107641691+720x@users.noreply.github.com>
2020-06-27 07:35:39 +0000

    minor typo fixed

--- app/src/main/java/com/harshit/imagesearch/activity/CartActivity.kt
+++ app/src/main/java/com/harshit/imagesearch/activity/CartActivity.kt
@@ -13,4 +13,6 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.harshit.imagesearch.models.CartModel
-import com.harshit.i
+import com.harshit.imagesearch.listeners.ICartLoadListener
+import com.harshit.imagesearch.R
+i
