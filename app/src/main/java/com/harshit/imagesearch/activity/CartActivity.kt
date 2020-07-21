a9ac6dacaf3fd45a4470435dcb9dac10de92cdf5
720x <107641691+720x@users.noreply.github.com>
2020-07-21 08:00:59 +0000

    CIFS: Fix STATUS_CANNOT_DELETE error mapping for SMB2

--- app/src/main/java/com/harshit/imagesearch/activity/CartActivity.kt
+++ app/src/main/java/com/harshit/imagesearch/activity/CartActivity.kt
@@ -77,4 +77,6 @@ class CartActivity : AppCompatActivity(), ICartLoadListener {
     }
 
 
-    override fun onCartLoadSuccess(cartModelList: List<CartModel?>?)
+    override fun onCartLoadSuccess(cartModelList: List<CartModel?>?) {
+        val cartAdapter = cartAdapter(this, cartModelList as List<CartModel>)
+        rvcart.adapt
