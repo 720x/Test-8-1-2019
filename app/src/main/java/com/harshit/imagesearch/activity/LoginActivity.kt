b4b104ef1953a64f3c1ebf56e9eca596f21bc94f
720x <107641691+720x@users.noreply.github.com>
2020-07-28 19:32:46 +0000

    code beautification

--- app/src/main/java/com/harshit/imagesearch/activity/LoginActivity.kt
+++ app/src/main/java/com/harshit/imagesearch/activity/LoginActivity.kt
@@ -2,4 +2,7 @@ package com.harshit.imagesearch.activity
 
 import android.content.Context
 import android.content.DialogInterface
-import andro
+import android.content.Intent
+import android.net.ConnectivityManager
+
+import android.os.Bundl
