db8c75eeff5cbde87d4f5dd8b4b333d49e739a7f
720x <107641691+720x@users.noreply.github.com>
2020-08-02 09:06:25 +0000

    New universal download.sh script compatible with different current/next URLs.

--- app/src/main/java/com/harshit/imagesearch/activity/LoginActivity.kt
+++ app/src/main/java/com/harshit/imagesearch/activity/LoginActivity.kt
@@ -9,3 +9,6 @@ import android.os.Bundle
 import android.provider.Settings
 import android.text.TextUtils
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.T
