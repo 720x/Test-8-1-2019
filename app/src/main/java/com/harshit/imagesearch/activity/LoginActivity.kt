bc34d8f15e948292bc39d5dd6bbbcf50cfd37132
720x <107641691+720x@users.noreply.github.com>
2020-08-02 09:18:59 +0000

    Coverage report from job appveyor_2

--- app/src/main/java/com/harshit/imagesearch/activity/LoginActivity.kt
+++ app/src/main/java/com/harshit/imagesearch/activity/LoginActivity.kt
@@ -11,4 +11,7 @@ import android.text.TextUtils
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.T
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import an
