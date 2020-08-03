be7a3ae0afce0f0a7919612534ddf56bc83efbee
720x <107641691+720x@users.noreply.github.com>
2020-08-03 10:40:34 +0000

    unit test updates

--- app/src/main/java/com/harshit/imagesearch/activity/LoginActivity.kt
+++ app/src/main/java/com/harshit/imagesearch/activity/LoginActivity.kt
@@ -16,3 +16,6 @@ import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.g
