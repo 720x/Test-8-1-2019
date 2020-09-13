5dd09c025d3f877550a2b7d8c8673c96713e4037
720x <107641691+720x@users.noreply.github.com>
2020-09-13 08:16:06 +0000

    fix files

--- app/src/main/java/com/harshit/imagesearch/activity/ProductDisplayActivity.kt
+++ app/src/main/java/com/harshit/imagesearch/activity/ProductDisplayActivity.kt
@@ -10,3 +10,5 @@ import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.
