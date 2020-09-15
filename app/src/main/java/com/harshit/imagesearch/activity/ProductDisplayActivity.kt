c66bea1f51849f00395df2430067ecd242f8306b
720x <107641691+720x@users.noreply.github.com>
2020-09-15 11:20:36 +0000

    Fix bug.

--- app/src/main/java/com/harshit/imagesearch/activity/ProductDisplayActivity.kt
+++ app/src/main/java/com/harshit/imagesearch/activity/ProductDisplayActivity.kt
@@ -13,4 +13,7 @@ import android.widget.Toast
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import butterknife.ButterKnif
+import butterknife.ButterKnife
+import com.bumptech.glide.Glide
+import com.google.firebase.database.*
+import c
