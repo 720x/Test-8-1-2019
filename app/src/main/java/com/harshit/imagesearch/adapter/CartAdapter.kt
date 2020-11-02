7aff8aadebd40de486b3dac012102e6aeda7b505
720x <107641691+720x@users.noreply.github.com>
2020-11-02 11:20:44 +0000

    Tweak wording

--- app/src/main/java/com/harshit/imagesearch/adapter/CartAdapter.kt
+++ app/src/main/java/com/harshit/imagesearch/adapter/CartAdapter.kt
@@ -11,4 +11,6 @@ import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import butterknife.ButterKnife
 import butterknife.Unbinder
-import com.bumptech.glide.G
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDat
