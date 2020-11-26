02a2d9d19ee438f39ac0eb0cb890be2651937a8e
720x <107641691+720x@users.noreply.github.com>
2020-11-26 15:21:27 +0000

    Update composer.lock.

--- app/src/main/java/com/harshit/imagesearch/adapter/ProductAdapter.kt
+++ app/src/main/java/com/harshit/imagesearch/adapter/ProductAdapter.kt
@@ -2,3 +2,7 @@ package com.harshit.imagesearch.adapter
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageVie
