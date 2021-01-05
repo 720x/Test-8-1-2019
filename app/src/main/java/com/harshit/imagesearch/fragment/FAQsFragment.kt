5cff7253b4ece73f9dcadc19b2774dedee1a21d1
720x <107641691+720x@users.noreply.github.com>
2021-01-05 19:46:40 +0000

    [se] CF

--- app/src/main/java/com/harshit/imagesearch/fragment/FAQsFragment.kt
+++ app/src/main/java/com/harshit/imagesearch/fragment/FAQsFragment.kt
@@ -4,4 +4,7 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
-import android
+import android.view.ViewGroup
+import com.harshit.imagesearch.R
+
+// TODO: Rename paramet
