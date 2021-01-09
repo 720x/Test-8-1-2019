24102f6b8699d75c8bea086c789c635b81ccfca9
720x <107641691+720x@users.noreply.github.com>
2021-01-09 08:41:39 +0000

    avoid warning

--- app/src/main/java/com/harshit/imagesearch/fragment/FAQsFragment.kt
+++ app/src/main/java/com/harshit/imagesearch/fragment/FAQsFragment.kt
@@ -8,4 +8,9 @@ import android.view.ViewGroup
 import com.harshit.imagesearch.R
 
 // TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g.
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * 
