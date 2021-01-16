b38d6e397dcaedca6c141898e1955ca3752dda95
720x <107641691+720x@users.noreply.github.com>
2021-01-16 08:12:28 +0000

    clean up build

--- app/src/main/java/com/harshit/imagesearch/fragment/FAQsFragment.kt
+++ app/src/main/java/com/harshit/imagesearch/fragment/FAQsFragment.kt
@@ -20,4 +20,7 @@ private const val ARG_PARAM2 = "param2"
 class FAQsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
-    private var param2: String? =
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(
