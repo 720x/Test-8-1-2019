3ce6fd4554b482800ba7b212e39fed262ba8a567
720x <107641691+720x@users.noreply.github.com>
2021-01-24 07:34:43 +0000

    versions update

--- app/src/main/java/com/harshit/imagesearch/fragment/FAQsFragment.kt
+++ app/src/main/java/com/harshit/imagesearch/fragment/FAQsFragment.kt
@@ -35,4 +35,9 @@ class FAQsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_f_a_qs, 
+        return inflater.inflate(R.layout.fragment_f_a_qs, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method
