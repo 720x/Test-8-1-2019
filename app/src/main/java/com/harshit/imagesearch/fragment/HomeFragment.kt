c099587497cb6c8ee4bdcd871f294494fec11f8a
720x <107641691+720x@users.noreply.github.com>
2021-02-27 21:51:23 +0000

    Fix tests compilation

--- app/src/main/java/com/harshit/imagesearch/fragment/HomeFragment.kt
+++ app/src/main/java/com/harshit/imagesearch/fragment/HomeFragment.kt
@@ -11,4 +11,8 @@ import com.harshit.imagesearch.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const v
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragm
