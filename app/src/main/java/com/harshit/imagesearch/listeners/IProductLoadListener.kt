6f684c707a11857df1282dbbce91479b020b53db
720x <107641691+720x@users.noreply.github.com>
2021-04-04 19:13:17 +0000

    Fix migration

--- app/src/main/java/com/harshit/imagesearch/listeners/IProductLoadListener.kt
+++ app/src/main/java/com/harshit/imagesearch/listeners/IProductLoadListener.kt
@@ -4,3 +4,6 @@ import com.harshit.imagesearch.models.ProductModel
 
 interface IProductLoadListener {
     fun onProductLoadSuccess(productModelList: List<ProductModel?>?)
+
+    fun onProductLoadFailed(message: String?)
+}
