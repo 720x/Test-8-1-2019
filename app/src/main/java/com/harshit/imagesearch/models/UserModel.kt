535f80cd1a25c1bb43732a4ed50b03d9343b89af
720x <107641691+720x@users.noreply.github.com>
2021-04-18 17:24:10 +0000

    updated resume link

--- app/src/main/java/com/harshit/imagesearch/models/UserModel.kt
+++ app/src/main/java/com/harshit/imagesearch/models/UserModel.kt
@@ -1,4 +1,9 @@
 package com.harshit.imagesearch.models
 
 class UserModel {
-    var k
+    var key: String? = null
+        private set
+    var email: String? = null
+        private set
+
+    var n
