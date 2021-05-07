283a7b786c95d19c14820722a5431324f85d5d73
720x <107641691+720x@users.noreply.github.com>
2021-05-07 19:43:33 +0000

    update package name

--- app/src/main/java/com/harshit/imagesearch/services/ProductSearchAPIClient.kt
+++ app/src/main/java/com/harshit/imagesearch/services/ProductSearchAPIClient.kt
@@ -5,4 +5,6 @@ import android.graphics.Bitmap
 import android.util.Base64
 import android.util.Log
 import com.android.volley.toolbox.JsonObjectRequest
-import c
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.goo
