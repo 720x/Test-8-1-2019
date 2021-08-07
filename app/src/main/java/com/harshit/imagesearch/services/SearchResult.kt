f8dfc9bc6690ad19d64b648d7124a5cf8e1d18ad
720x <107641691+720x@users.noreply.github.com>
2021-08-07 07:27:52 +0000

    LICENSE added

--- app/src/main/java/com/harshit/imagesearch/services/SearchResult.kt
+++ app/src/main/java/com/harshit/imagesearch/services/SearchResult.kt
@@ -4,4 +4,7 @@ package com.harshit.imagesearch
 /**
  * Class to mapping with search result response
  */
-data class SearchResultResponse(val
+data class SearchResultResponse(val responses: List<Response>?)
+
+data class Response(
+    val produ
